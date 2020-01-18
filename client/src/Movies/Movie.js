c1f5388a8a4814359ae73777129b97407da4c9d0
Trevor Thompson <trevthom18@gmail.com>
2020-01-17 19:50:05 -0500

    Successfully managed to push up both stretch assignments

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
+import { Link } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
@@ -23,6 +25,7 @@ const Movie = (props) => {
 
   const saveMovie = () => {
     const addToSavedList = props.addToSavedList;
+    // I'm unsure of how to do this. I will ask for help later because I would really like to know how to do this
     addToSavedList(movie)
   }
 
@@ -30,26 +33,10 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button" onClick={saveMovie}>Save</div>
+    <div>
+      <MovieCard movie={movie}/>
+      <Link to={`/`}><div className="save-button" onClick={saveMovie}>Save</div></Link>
     </div>
   );
 }
